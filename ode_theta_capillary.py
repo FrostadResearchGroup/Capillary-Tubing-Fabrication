3702dbb17f9404790be2695eaaf6625dab6506d3
frostad-john <john.m.frostad@gmail.com>
2017-05-25 17:41:27 -0700

    Add files via upload
    Created input parameter section.
    Changed variable names and function names.
    Corrected formula for computing derivatives in system of odes.
    Added some plot handling code and added additional plot.

--- ode_theta_capillary.py
+++ ode_theta_capillary.py
@@ -6,24 +6,42 @@ Created on Tue May 23 19:21:40 2017
 """
 
 import numpy as np
-import scipy
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def myfun(X,x,P):
-    
-    E=16666.7
-    I=8.15499e-16
-    
-    theta=X[0]
-    x2=X[1]
-    
-    f=[np.tan(theta),(-(P*x)/(E*I))*(1+x2**2)**(3/2)]
-    return f
+# INPUT PARAMETERS
+y0 = [0.5,0] # [initial position at x=0,derivative at x=0]
+H = 0.2 # Distance of cantilever in meters
+E = 3.95e9 # Young's modulus in Pa
+I = 8.15499e-16 # Moment of intertia for cantilever
+P = .0001 # Force on cantilever in N
+N = 2000 # Number of points in intergration
 
-x0=[0.001,0.001]
-x=np.linspace(0,0.194,20)
-P=.0003
-output=odeint(myfun,x0,x,(P,))
+def cantilever_ode_system(y,x,P,E,I):
+    
+    dy0_dx = y[1]
+    dy1_dx = -P*x/E/I*(1+y[1]**2)**(3/2)
+    
+    dy_dx = [dy0_dx , dy1_dx]
+    
+    return dy_dx
+    
+    
+plt.close('all')
 
-plt.plot(output[:,0],x)
+x = np.linspace(0,H,N)
+output = odeint(cantilever_ode_system,y0,x,args=(P,E,I))
+
+y = output[:,0]
+dy_dx = output[:,1]
+# Compute theta in degrees
+theta = np.arctan(dy_dx)/np.pi*180
+
+# Plotting y vs. x so that it looks like a vertical beam
+plt.figure()
+plt.plot(y,-x)
+plt.axis('equal')
+
+plt.figure()
+plt.plot(x,-theta)
+#plt.axis('equal')
