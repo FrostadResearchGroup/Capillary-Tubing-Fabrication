7ddd780d2477917e03e405bd2896b6fe35d5e64a
FrostadResearch <frostadresearch@gmail.com>
2017-05-31 17:51:53 -0700

    Add files via upload
    modified to include arc length calculation

--- copy_of_ode_theta_capillary.py
+++ copy_of_ode_theta_capillary.py
@@ -5,12 +5,13 @@ Created on Fri May 26 17:36:06 2017
 @author: jlapucha
 """
 
-#copy of ode_capillary_theta.py to modify I
-
+#copy of ode_capillary_theta.py to modify I 
+#includes arc length calculations
 
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 # INPUT PARAMETERS
 y0 = [0.5,0] # [initial position at x=0,derivative at x=0]
@@ -21,7 +22,7 @@ R2 = 51.8e-6 #ID of capillary tubing
 P = .0001 #Force on Cantilever in N
 N = 2000 # Number of points in intergration
 
-def second_moment(I, R1, R2):
+def moment(I, R1, R2):
     I= np.pi/4(R2**4-R1**4)
     return I
 
@@ -34,16 +35,30 @@ def cantilever_ode_system(y,x,P,E,I):
     
     return dy_dx
     
+def integrand(dy_dx):
     
+     return np.sqrt(1+(dy_dx)**2)
+     
+     
+
+     
 plt.close('all')
 
 x = np.linspace(0,H,N)
 output = odeint(cantilever_ode_system,y0,x,args=(P,E,I))
 
+
 y = output[:,0]
 dy_dx = output[:,1]
+
+
+#compute arc length via integration
+output_length= quad(integrand,0,0.2)
+
+
 # Compute theta in degrees
 theta = np.arctan(dy_dx)/np.pi*180
+
 
 # Plotting y vs. x so that it looks like a vertical beam
 plt.figure()
@@ -53,3 +68,10 @@ plt.axis('equal')
 plt.figure()
 plt.plot(x,-theta)
 #plt.axis('equal')
+
+
+
+
+    
+
+
